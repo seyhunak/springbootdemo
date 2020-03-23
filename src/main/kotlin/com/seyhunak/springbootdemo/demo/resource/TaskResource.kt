14cf3eb8f1038d17cdefc496eff8e65ed0630a40
SEYHUN AKYÜREK <seyhun.akyurek@consultant.turkcell.com.tr>
2020-03-23 17:40:46 +0300

    initial

---
+++ src/main/kotlin/com/seyhunak/springbootdemo/demo/resource/TaskResource.kt
@@ -0,0 +1,35 @@
+package com.seyhunak.springbootdemo.resource
+
+import com.seyhunak.springbootdemo.model.Task
+import com.seyhunak.springbootdemo.service.TaskService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import javax.validation.Valid
+
+@RestController
+@RequestMapping("/v1/api/tasks")
+class TaskResource(private val taskService: TaskService) {
+
+  @GetMapping
+  fun getTasks(): List<Task> =
+    taskService.getTasks()
+
+  @PostMapping
+  fun addTask(@Valid @RequestBody task: Task): ResponseEntity<Task> =
+    taskService.addTask(task)
+
+  @GetMapping("/{id}")
+  fun getTaskById(@PathVariable(value="id") taskId: Long): ResponseEntity<Task> =
+    taskService.getTaskById(taskId)
+
+  @PutMapping("/{id}")
+  fun updateTaskById(
+    @PathVariable(value = "id") taskId: Long,
+    @Valid @RequestBody newTask: Task): ResponseEntity<Task> =
+    taskService.putTask(taskId, newTask)
+
+  @DeleteMapping("/{id}")
+  fun deleteTask(@PathVariable(value = "id") taskId: Long): ResponseEntity<Void> =
+    taskService.deleteTask(taskId)
+}
---
+++ src/main/resources/db/migration/V2__TestData.sql
@@ -0,0 +1,2 @@
+insert into tasks(title, status, priority, description) values ('First Task', 1, 1, 'My first task description');
+insert into tasks(title, status, priority, description) values ('Another Task', 2, 2, 'Another task description');
